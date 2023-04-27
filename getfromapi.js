bfaa1b73bf72a996a03a9e75401506d4d3b05026
Stenoliv <stenbaco@arcada.fi>
2023-04-27 21:44:48 +0300

    Adds Weather api enhances drinks api

--- getfromapi.js
+++ getfromapi.js
@@ -1,27 +1,34 @@
 async function getboredapi(){
-    //try {
-    const resp = await fetch ("https://www.boredapi.com/api/activity");
-    const respJson = await resp.json();
-
+    try {
+        const resp = await fetch ("https://www.boredapi.com/api/activity");
+        const respJson = await resp.json();
         document.querySelector('#bored').innerHTML += `
         <h2>Are you bored? Here is a random activity</h2><p>${respJson.activity}</p><p>The price is: ${respJson.price}€</p><p>Participants: ${respJson.participants} </p><a>${respJson.link}</a> `;
-/*}catch {
-    document.querySelector('#bored').innerHTML = `Error while trying to get activity!`
-}*/
+}catch {
+    document.querySelector('#bored').innerHTML = `<p>Error while trying to get activity!</p>`
+}
 }
 getboredapi();
 
+
 document.querySelector("#searchfordrink").addEventListener("click",e => {getdrinkapi()});
-    //try{
     async function getdrinkapi(){
-    $drink = document.getElementById('drinkname').value;
-    const resp = await fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+$drink);
-    const respJson = await resp.json();
+    try{
+        $drink = document.getElementById('drinkname').value;
+        const resp = await fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+$drink);
+        const respJson = await resp.json();
 
-    document.querySelector('#drinkrecepie').innerHTML =`
-    <h2>Your drink is here</h2><p>${respJson.drinks[0].strDrink}</p><p>Instructions: ${respJson.drinks[0].strInstructions}</p>`
-    }
-    /*}catch{
+        document.querySelector('#drinkrecepie').innerHTML =`
+        <h2>Your drink is here</h2><p>Name: ${respJson.drinks[0].strDrink}</p><p>It belongs in category: ${respJson.drinks[0].strCategory}</p><p>Instructions: ${respJson.drinks[0].strInstructions}</p>`
+   
+        for(let i = 1; i<=15; i++){
+            var list = document.querySelector("#drinkrecepie");
+            ingredient = respJson.drinks[0]["strIngredient" + i]
+            measurment = respJson.drinks[0]["strMeasure" + i]
+            if(ingredient != null && measurment != null)list.innerHTML +=`<p>${ingredient} ${measurment}</p>` 
+            else if(ingredient != null)list.innerHTML +=`<p>${ingredient}</p>` 
+        }
+    }catch{
         document.querySelector('#drinkrecepie').innerHTML =`Error while trying to get drink!`
-    }*/
-
+    }
+    }
--- weather_api.js
+++ weather_api.js
@@ -1,1 +1,38 @@
-console.log("hello");
+let userLocation = navigator.geolocation;
+function lonlat() {
+    if(userLocation) {
+        userLocation.getCurrentPosition(located);
+    }else {
+        "Could not get location!";
+    }
+    }
+function located(data) {
+    let lat = data.coords.latitude;
+    let long = data.coords.longitude;
+
+    console.log("coordinates",lat,long);
+async function getWeather(){
+    try {
+    API_key = localStorage.getItem("weather_api_key");
+    const resp = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_key}`);
+    const respJson = await resp.json();
+    let degree = respJson.wind.deg; 
+    //lånad kod för att ändra från grader till väderstreck (N,E,S,W)
+    function degToCompass() {
+        var val = Math.floor((degree / 22.5) + 0.5);
+        var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+        return arr[(val % 16)];
+    }
+    //lånad kod slut
+        weatherid = respJson.weather[0].icon;
+        document.querySelector('#weather').innerHTML += `
+        <h2>Weather at ${respJson.name}</h2><p>${respJson.main.temp}℃</p><p>Feels like ${respJson.main.feels_like}℃</p><p>${respJson.wind.speed}m/s ${degToCompass()}</p><p>Humidity ${respJson.main.humidity}%</p><img src=' https://openweathermap.org/img/wn/${weatherid}@2x.png'></img><p>${respJson.weather[0].description}</p> `;
+}catch {
+    document.querySelector('#weather').innerHTML = `Error while trying to get weather!`
+}
+}
+getWeather()
+}
+lonlat();
+
+    
