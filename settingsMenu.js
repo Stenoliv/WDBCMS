fdad3ea38e8f4d34783fbfa5a8c92b2d57b2c53e
Ranzz02 <rasmus.raiha@hotmail.com>
2023-05-08 16:46:48 +0300

    Added todo app request new profile

--- settingsMenu.js
+++ settingsMenu.js
@@ -1,9 +1,13 @@
 const ChatGPT_API_KEY_STORAGE = "chatgpt_api_key";
 const WEATHER_API_KEY_STORAGE = "weather_api_key";
+const ToDo_API_KEY_STORAGE = "todo_api_key";
+const ToDo_API_USERNAME_STORAGE = "todo_api_username";
 
 const settingsMneu = document.getElementById("menu-settings");
 const chatgptAPI = document.getElementById("chatgpt-api-key");
 const weatherAPI = document.getElementById("weather-api-key");
+const todoAPI = document.getElementById("todo-api-key");
+const todoUsername = document.getElementById("todo-api-username");
 
 function changeMenu() {
     if (settingsMneu.classList.contains("hidden")) {
@@ -19,9 +23,13 @@ function updateAPIkey(update) {
     if (update) {
         localStorage.setItem(WEATHER_API_KEY_STORAGE, weatherAPI.value);
         localStorage.setItem(ChatGPT_API_KEY_STORAGE, chatgptAPI.value);
+        localStorage.setItem(ToDo_API_KEY_STORAGE, todoAPI.value);
+        localStorage.setItem(ToDo_API_USERNAME_STORAGE, todoUsername.value);
     } else {
         weatherAPI.value = localStorage.getItem(WEATHER_API_KEY_STORAGE);
         chatgptAPI.value = localStorage.getItem(ChatGPT_API_KEY_STORAGE);
+        todoAPI.value = localStorage.getItem(ToDo_API_KEY_STORAGE);
+        todoUsername.value = localStorage.getItem(ToDo_API_USERNAME_STORAGE);
     }
 }
 
--- todoapp.js
+++ todoapp.js
@@ -1,6 +1,37 @@
-/*api_Key = localStorage.getItem(api_key);
-user_Name = localStorage.getItem(user_name);
-*/
+const TODO_API_URL = "http://128.214.253.222:8355";
+
+let usernameHTML = document.getElementById("username");
+let emailHTML = document.getElementById("email");
+var API_KEY = localStorage.getItem(ToDo_API_KEY_STORAGE);
+var USERNAME = localStorage.getItem(ToDo_API_USERNAME_STORAGE);
+
+if (USERNAME != null) usernameHTML.value = USERNAME;
+
+async function request_new_api() {
+    body = {
+        username: usernameHTML.value,
+        api_key: API_KEY,
+        email: emailHTML.value
+    }
+    
+    await fetch (TODO_API_URL+"/todos/register", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)})
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.hasOwnProperty('api_key')) {
+                localStorage.setItem(ToDo_API_KEY_STORAGE,data.api_key);
+                localStorage.setItem(ToDo_API_USERNAME_STORAGE,usernameHTML.value);
+                API_KEY = data.api_key;
+                USERNAME = usernameHTML.value;
+            }
+        })
+        .catch(error => {
+            console.log("ERROR: Failed")
+        })
+}
+
 async function getTodoOptions(){
     try{
         const resp = await fetch ("http://128.214.253.222:8438/todos/categories");
@@ -17,7 +48,7 @@ getTodoOptions()
 
 async function getToDos(){
     try{
-        const resp = await fetch("http://128.214.253.222:8438/todos/?api_key="+api_Key+"&"+user_Name);
+        const resp = await fetch("http://128.214.253.222:8438/todos/?api_key="+API_KEY+"&"+USERNAME);
         const respJson = await resp.json();
         
     }
