fafe7de7830156afdbc95d71a144619b86faf2f1
Ranzz02 <rasmus.raiha@hotmail.com>
2023-05-09 14:49:54 +0300

    BUG: fixed not able to close edit box

--- todoapp.js
+++ todoapp.js
@@ -9,10 +9,10 @@ var edit_id;
 var edit_div;
 
 var newToDoEntry = {
-    title: null,
-    due_date: null,
-    done: null,
-    category_id: null
+    title:null,
+    due_date:null,
+    done:null,
+    category_id:null
 };
 
 if (USERNAME != null) usernameHTML.value = USERNAME;
@@ -23,17 +23,16 @@ async function request_new_api() {
         api_key: API_KEY,
         email: emailHTML.value
     }
-
-    await fetch(TODO_API_URL + "/todos/register", {
+    
+    await fetch (TODO_API_URL+"/todos/register", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body)
-    })
+        body: JSON.stringify(body)})
         .then(resp => resp.json())
         .then(data => {
             if (data.hasOwnProperty('api_key')) {
-                localStorage.setItem(ToDo_API_KEY_STORAGE, data.api_key);
-                localStorage.setItem(ToDo_API_USERNAME_STORAGE, usernameHTML.value);
+                localStorage.setItem(ToDo_API_KEY_STORAGE,data.api_key);
+                localStorage.setItem(ToDo_API_USERNAME_STORAGE,usernameHTML.value);
                 API_KEY = data.api_key;
                 USERNAME = usernameHTML.value;
             }
@@ -43,23 +42,23 @@ async function request_new_api() {
         })
 }
 
-async function getTodoOptions() {
-    try {
-        const resp = await fetch(TODO_API_URL + "/todos/categories");
+async function getTodoOptions(){
+    try{
+        const resp = await fetch (TODO_API_URL+"/todos/categories");
         const respJson = await resp.json();
-        for (let i = 0; Object.keys(respJson.result).length > i; i++) {
+        for(let i = 0 ; Object.keys(respJson.result).length > i ; i++){
             document.querySelector("#todocategory").innerHTML += `<option value="${respJson.result[i].id}">${respJson.result[i].category_name}</option>`;
         }
     }
-    catch {
+    catch{
         document.querySelector("#newtodo").innerHTML += `<p>Something went wrong while getting categories!</p>`;
     }
 }
 getTodoOptions()
 
-async function getToDos() {
-    try {
-        const resp = await fetch(TODO_API_URL + "/todos?api_key=" + API_KEY + "&username=" + USERNAME);
+async function getToDos(){
+    try{
+        const resp = await fetch(TODO_API_URL+"/todos?api_key="+API_KEY+"&username="+USERNAME);
         const respJson = await resp.json();
         if (!Array.isArray(respJson.result)) {
             throw new Error(respJson.result)
@@ -69,23 +68,26 @@ async function getToDos() {
 
         document.querySelector("#toDoList").innerHTML = ""
         respJson.result.forEach(elem => {
-            let jaahas = "";
-            jaahas += `<div 
+            let jaahas ="";
+            jaahas +=`<div 
             data-todo_id="${elem.id}" 
             data-todo_category="${elem.category_name}" 
             data-todo_done="${elem.done}"
             data-todo_title="${elem.title}"
             data-todo_duedate="${elem.due_date}"
             class="toDoItem"><p class="todo-title">${elem.title}</p><span class="categoryTag ${elem.category_name}">${elem.category_name}</span><p class="todo-due_date"> Due: ${elem.due_date}</p>`
-            jaahas += `<div class="edit-todo"><img class="edit-todo-img" src="./wrench-24.png"></div>`
-            jaahas += `<div class="tooltip"><span class="tooltiptext `
+            jaahas+=`<div class="edit-todo"><img class="edit-todo-img" src="./wrench-24.png"></div>`
+            jaahas+=`<div class="tooltip"><span class="tooltiptext `
 
-            if (new Date(elem.due_date) < new Date() && elem.done == false) {
-                jaahas += ` taskOverdue">Status: </span><img src="./error-24.png" class="taskOverdue"></img></div>`
-            } else if (elem.done == false) {
-                jaahas += ` notDoneTask">Status: </span><img src="./warning-4-24.png" class="notDoneTask"></img></div>`
-            } else {
-                jaahas += ` doneTask">Status: </span><img src="./ok-24.png" class="doneTask"></img></div>`;
+            if (new Date(elem.due_date) < new Date() && elem.done == false) 
+            {
+                jaahas +=` taskOverdue">Status: </span><img src="./error-24.png" class="taskOverdue"></img></div>`
+            } else if (elem.done == false) 
+            {
+                jaahas +=` notDoneTask">Status: </span><img src="./warning-4-24.png" class="notDoneTask"></img></div>`
+            } else 
+            {
+                jaahas+=` doneTask">Status: </span><img src="./ok-24.png" class="doneTask"></img></div>`;
             }
             jaahas += `<div class="tooltip"><span class="tooltiptext">Delete</span><img src="./x-mark-4-24.png" class="deleteTask"></div></div>`
             document.querySelector("#toDoList").innerHTML += jaahas
@@ -95,27 +97,25 @@ async function getToDos() {
         document.querySelectorAll(".toDoItem").forEach(div => {
             try {
                 // If not checked query for check done 
-                div.querySelector("img.notDoneTask").addEventListener('click', () => {
-                    const data = { done: true }
-                    edit_div = div
-                    edit_id = div.dataset.todo_id
-                    checkDone(div.dataset.todo_id, data)
-                })
-            }
-            catch { }
+            div.querySelector("img.notDoneTask").addEventListener('click', () => {
+                const data = {done:true}
+                edit_div = div
+                edit_id = div.dataset.todo_id
+                checkDone(div.dataset.todo_id,data)
+            })}
+            catch {}
             try {
                 // If not checked query for check done 
                 div.querySelector("img.taskOverdue").addEventListener('click', () => {
-                    const data = { done: true }
-                    edit_div = div
-                    edit_id = div.dataset.todo_id
-                    checkDone(div.dataset.todo_id, data)
-                })
-            }
-            catch { }
+                const data = {done:true}
+                edit_div = div
+                edit_id = div.dataset.todo_id
+                checkDone(div.dataset.todo_id,data)
+            })}
+            catch {}
             // DELETE task
             div.querySelector("img.deleteTask").addEventListener('click', () => {
-                if (confirm("You sure you want to delete this task?")) {
+                if(confirm("You sure you want to delete this task?")) {
                     edit_div = div
                     edit_id = div.dataset.todo_id
                     deleteTask(div.dataset.todo_id)
@@ -130,7 +130,7 @@ async function getToDos() {
             })
         })
     }
-    catch (error) {
+    catch(error){
         if (error) document.querySelector("#toDoList").innerHTML = `<h3>${error}</h3>`;
         else document.querySelector("#toDoList").innerHTML = `<h3>Something went wrong!</h3>`;
     }
@@ -138,7 +138,7 @@ async function getToDos() {
 getToDos();
 
 async function checkDone(id, new_data) {
-    await fetch(TODO_API_URL + "/todos/" + id + "?api_key=" + API_KEY + "&username=" + USERNAME, {
+    await fetch(TODO_API_URL+"/todos/"+id+"?api_key="+API_KEY+"&username="+USERNAME, {
         method: "PATCH",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(new_data)
@@ -153,7 +153,7 @@ async function checkDone(id, new_data) {
 }
 
 async function deleteTask(id) {
-    await fetch(TODO_API_URL + "/todos/" + id + "?api_key=" + API_KEY + "&username=" + USERNAME, {
+    await fetch(TODO_API_URL+"/todos/"+id+"?api_key="+API_KEY+"&username="+USERNAME, {
         method: "DELETE",
         headers: { 'Content-Type': 'application/json' }
     })
@@ -184,7 +184,7 @@ async function openEditBox() {
     const done = document.querySelector("#todo_edit_done")
 
     category.innerHTML = ""
-    await fetch(TODO_API_URL + "/todos/categories")
+    await fetch(TODO_API_URL+"/todos/categories")
         .then(resp => resp.json())
         .then(data => {
             let index = 0;
@@ -200,30 +200,34 @@ async function openEditBox() {
             console.log(error)
         })
 
-    title.value = todoEntry.title
-    duedate.value = todoEntry.due_date
-    done.checked = todoEntry.done
+        title.value = todoEntry.title
+        duedate.value = todoEntry.due_date
+        done.checked = todoEntry.done
+    
+        title.addEventListener('change', () => {
+            newToDoEntry.title = title.value;
+        })
+        category.addEventListener('change', () => {
+            newToDoEntry.category_id = category.value;
+        })
+        duedate.addEventListener('change', () => {
+            newToDoEntry.due_date = duedate.value
+        })
+        done.addEventListener('change', () => {
+            newToDoEntry.done = done.checked
+        })
+        document.querySelector("#todo_edit_submit").removeEventListener('click', submitChanges)
+        document.querySelector("#todo_edit_submit").addEventListener('click', submitChanges)
+    
+        document.querySelector(".todo_edit_box").classList.remove("hidden")
+}
 
-    title.addEventListener('change', () => {
-        newToDoEntry.title = title.value;
-    })
-    category.addEventListener('change', () => {
-        newToDoEntry.category_id = category.value;
-    })
-    duedate.addEventListener('change', () => {
-        newToDoEntry.due_date = duedate.value
-    })
-    done.addEventListener('change', () => {
-        newToDoEntry.done = done.checked
-    })
-    document.querySelector("#todo_edit_submit").removeEventListener('click', submitChanges)
-    document.querySelector("#todo_edit_submit").addEventListener('click', submitChanges)
-
-    document.querySelector(".todo_edit_box").classList.remove("hidden")
+function closeEditBox() {
+    document.querySelector(".todo_edit_box").classList.add("hidden")
 }
 
 async function updateToDoTask(id, changedData) {
-    await fetch(TODO_API_URL + "/todos/" + id + "?api_key=" + API_KEY + "&username=" + USERNAME, {
+    await fetch(TODO_API_URL+"/todos/"+id+"?api_key="+API_KEY+"&username="+USERNAME, {
         method: "PATCH",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(changedData)
@@ -238,17 +242,17 @@ async function updateToDoTask(id, changedData) {
 }
 
 document.querySelector("#sumbitnewtaskbtn").addEventListener('click', newToDoTask);
-async function newToDoTask() {
+async function newToDoTask(){
     let title = document.querySelector("#newToDotitle").value;
     let category = document.querySelector("#todocategory").value;
     let newduedate = document.querySelector("#dueDateInput").value;
     newduedate = new Date(newduedate);
-    let inputdata = { title: title, category_id: category, due_date: newduedate }
+    let inputdata = {title:title,category_id:category, due_date:newduedate}
     console.log(inputdata);
-
-    await fetch(TODO_API_URL + "/todos" + "?api_key=" + API_KEY + "&username=" + USERNAME, {
+    
+    await fetch(TODO_API_URL+"/todos"+"?api_key="+API_KEY+"&username="+USERNAME,{
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers:{'Content-Type': 'application/json' }, 
         body: JSON.stringify(inputdata)
     })
         .then(resp => resp.json())
@@ -260,7 +264,7 @@ async function newToDoTask() {
 function submitChanges() {
     console.log("submit!")
     updateToDoTask(edit_id, newToDoEntry)
-    document.querySelector(".todo_edit_box").classList.add("hidden")
+    closeEditBox()
     return;
 }
 
@@ -327,63 +331,63 @@ function addNewDiv(data) {
     const toDoList = document.querySelector("#toDoList");
 
     let newDiv = "";
-    newDiv += `<div 
+    newDiv +=`<div 
             data-todo_id="${data.result.id}" 
             data-todo_category="${data.result.category_name}" 
             data-todo_done="${data.result.done}"
             data-todo_title="${data.result.title}"
             data-todo_duedate="${data.result.due_date}"
             class="toDoItem"><p class="todo-title">${data.result.title}</p><span class="categoryTag ${data.result.category_name}">${data.result.category_name}</span><p class="todo-due_date"> Due: ${data.result.due_date}</p>`
-    newDiv += `<div class="edit-todo"><img class="edit-todo-img" src="./wrench-24.png"></div>`
-    newDiv += `<div class="tooltip"><span class="tooltiptext `
+            newDiv+=`<div class="edit-todo"><img class="edit-todo-img" src="./wrench-24.png"></div>`
+            newDiv+=`<div class="tooltip"><span class="tooltiptext `
 
-    if (new Date(data.result.due_date) < new Date() && data.result.done == false) {
-        newDiv += ` taskOverdue">Status: </span><img src="./error-24.png" class="taskOverdue"></img></div>`
-    } else if (data.result.done == false) {
-        newDiv += ` notDoneTask">Status: </span><img src="./warning-4-24.png" class="notDoneTask"></img></div>`
-    } else {
-        newDiv += ` doneTask">Status: </span><img src="./ok-24.png" class="doneTask"></img></div>`;
-    }
-    newDiv += `<div class="tooltip"><span class="tooltiptext">Delete</span><img src="./x-mark-4-24.png" class="deleteTask"></div></div>`
+            if (new Date(data.result.due_date) < new Date() && data.result.done == false) 
+            {
+                newDiv +=` taskOverdue">Status: </span><img src="./error-24.png" class="taskOverdue"></img></div>`
+            } else if (data.result.done == false) 
+            {
+                newDiv +=` notDoneTask">Status: </span><img src="./warning-4-24.png" class="notDoneTask"></img></div>`
+            } else 
+            {
+                newDiv+=` doneTask">Status: </span><img src="./ok-24.png" class="doneTask"></img></div>`;
+            }
+            newDiv += `<div class="tooltip"><span class="tooltiptext">Delete</span><img src="./x-mark-4-24.png" class="deleteTask"></div></div>`
 
     toDoList.innerHTML += newDiv
-
     // Button listeners
-    document.querySelectorAll(".toDoItem").forEach(div => {
-        try {
-            // If not checked query for check done 
+        document.querySelectorAll(".toDoItem").forEach(div => {
+            try {
+                // If not checked query for check done 
             div.querySelector("img.notDoneTask").addEventListener('click', () => {
-                const data = { done: true }
+                const data = {done:true}
                 edit_div = div
                 edit_id = div.dataset.todo_id
-                checkDone(div.dataset.todo_id, data)
+                checkDone(div.dataset.todo_id,data)
+            })}
+            catch {}
+            try {
+                // If not checked query for check done 
+                div.querySelector("img.taskOverdue").addEventListener('click', () => {
+                const data = {done:true}
+                edit_div = div
+                edit_id = div.dataset.todo_id
+                checkDone(div.dataset.todo_id,data)
+            })}
+            catch {}
+            // DELETE task
+            div.querySelector("img.deleteTask").addEventListener('click', () => {
+                if(confirm("You sure you want to delete this task?")) {
+                    edit_div = div
+                    edit_id = div.dataset.todo_id
+                    deleteTask(div.dataset.todo_id)
+                }
             })
-        }
-        catch { }
-        try {
-            // If not checked query for check done 
-            div.querySelector("img.taskOverdue").addEventListener('click', () => {
-                const data = { done: true }
-                edit_div = div
+            // UPDATE task
+            div.querySelector(".edit-todo-img").addEventListener("click", () => {
+                console.log("OPEN EDIT BOX")
                 edit_id = div.dataset.todo_id
-                checkDone(div.dataset.todo_id, data)
+                edit_div = div
+                openEditBox()
             })
-        }
-        catch { }
-        // DELETE task
-        div.querySelector("img.deleteTask").addEventListener('click', () => {
-            if (confirm("You sure you want to delete this task?")) {
-                edit_div = div
-                edit_id = div.dataset.todo_id
-                deleteTask(div.dataset.todo_id)
-            }
         })
-        // UPDATE task
-        div.querySelector(".edit-todo-img").addEventListener("click", () => {
-            console.log("OPEN EDIT BOX")
-            edit_id = div.dataset.todo_id
-            edit_div = div
-            openEditBox()
-        })
-    })
 }
